8605564f05207e15c0c5667f78f5fd206acd806a
Pedro Reis <126337464+P3droReis@users.noreply.github.com>
2023-07-01 18:02:16 -0300

    Exercícios e Atividades de JS

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 79.0
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 80){ //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+} 
+console.log(`Dirija sempre com o cinto de segurança`)
