a530f83029702b2310fd2f0b72855c515ec74c7b
Pedro Reis <126337464+P3droReis@users.noreply.github.com>
2023-09-03 11:56:22 -0300

    atualização

--- Aula12exer/script.js
+++ Aula12exer/script.js
@@ -2,14 +2,14 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('foto')
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
-
-    if (hora >= 0 && hora < 12) {
-        img.src = 'manha.jpg'
-    } else if (hora >= 12 && hora < 18){
-        
-    }else{
-
+    //var hora = data.getHours()
+    var hora = 15
+    msg.innerHTML = `Agora são ${hora} horas` 
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+    }else if (hora >= 12 && hora <18){
+        img.src = 'tarde.png'
+    } else{
+        img.src = 'noite.png'
     }
 }
