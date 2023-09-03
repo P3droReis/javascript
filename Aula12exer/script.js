4f6466cba215ea4f7c224f040ad17212b4098de2
Pedro Reis <126337464+P3droReis@users.noreply.github.com>
2023-09-03 11:40:45 -0300

    atualização de nomes

---
+++ Aula12exer/script.js
@@ -0,0 +1,15 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12) {
+        img.innerHTML = img
+    } else if (hora >= 12 && hora < 18){
+        
+    }else{
+
+    }
+}
