2e10eea9b7d9a89a94a311192b40bd189c313015
Pedro Reis <126337464+P3droReis@users.noreply.github.com>
2023-09-16 18:15:22 -0300

    Inicio do exercício 14

---
+++ Aula14exer/ex016/script.js
@@ -0,0 +1,16 @@
+function contar() {
+    var camp1 = document.getElementById('inici')
+    var camp2 = document.getElementById('ifim')
+    var camp3 = document.getElementById('ipaso')
+    var res = document.getElementById('result')
+    
+    if (camp1.value == 0 || camp2.value == 0 || camp3.value == 0) {
+        alert('assim não da')
+    } else {
+        alert('tudo ok')
+    }
+
+    for(;;){
+
+    }
+}
