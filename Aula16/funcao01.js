0f789040523a2005f8bf6a6bdb1f66f5e66ad157
Pedro Reis <126337464+P3droReis@users.noreply.github.com>
2023-09-23 19:20:47 -0300

    exercícios de funções em JS

---
+++ Aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+console.log(parimp(45))
+
+/*
+let res = parimp(11)
+console.log(res)
+*/
